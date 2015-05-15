16a0bf8d95dfad98c8e41c5ba10f072f532b5d4f
Shilin Victor <chrono.monochrome@gmail.com>
2015-05-15 05:20:25 +0300

    Add new patcher and patchset
    Change-Id: Id87e41a8b848ebfa3e6a7f1533ee5c969e3d747b

---
+++ patches/apply.sh
@@ -0,0 +1,61 @@
+LOCAL_PATH=../../../..
+
+pre_clean() {
+    tmp=$PWD
+    cd $1
+
+    # get rid of any uncommitted or unstaged changes
+    git reset --hard
+    git clean -fd
+    git am --abort
+
+    cd $tmp
+}
+
+apply_all() {
+    tmp=$PWD
+    cd $1
+
+    for i in $( ls *.patch )
+    do
+        git am $i
+    done
+ 
+    cd $tmp
+}
+
+# pre clean 
+
+pre_clean bionic
+pre_clean build
+pre_clean frameworks/av/
+pre_clean frameworks/base/
+pre_clean frameworks/native/
+pre_clean frameworks/opt/net/wifi/
+pre_clean frameworks/opt/telephony/
+pre_clean packages/apps/Bluetooth/
+pre_clean packages/apps/Settings/
+pre_clean packages/services/Telecomm
+pre_clean packages/services/Telephony
+pre_clean system/core
+
+
+# copy patches
+cp -r * $LOCAL_PATH 
+cd $LOCAL_PATH
+
+# now apply it 
+
+apply_all bionic 
+apply_all build
+apply_all frameworks/av/
+apply_all frameworks/base/
+apply_all frameworks/native/
+apply_all frameworks/opt/net/wifi/
+apply_all frameworks/opt/telephony/
+apply_all packages/apps/Bluetooth/ 
+apply_all packages/apps/Settings/
+apply_all packages/services/Telecomm
+apply_all packages/services/Telephony
+apply_all system/core
+
